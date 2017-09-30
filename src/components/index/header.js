2abc474acf8cae00848ceb8d4372e7846903c859
szy0syz <szy0syz@gmail.com>
2017-09-30 10:25:23 +0800

    updated Index-component

---
+++ src/components/index/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+export default function() {
+  return(
+    <div className="navbar navbar-inserse">
+      <div className="narbar-inner">
+        <div className="container">
+          <a href="/" className="brand">
+            <img src="./img/grunt-logo.png" alt="logo"/>
+          </a>
+        </div>
+      </div>
+    </div>  
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import Index from './components/index/index';
 import registerServiceWorker from './registerServiceWorker';
 
